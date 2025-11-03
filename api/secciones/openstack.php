af2d58a32cd94ab2a622a897e991afb748b5d526
luxo11GB <gzalesluis3@gmail.com>
2025-11-03 18:31:59 -0500

    Update OpenStack news details in openstack.php

--- api/secciones/openstack.php
+++ api/secciones/openstack.php
@@ -4,8 +4,9 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $openstack = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "OpenStack sigue ganando terreno en la infraestructura cloud",
+    "autor" => "Javier Rodríguez",
+    "resumen" => "OpenStack, la plataforma de infraestructura en la nube de código abierto, continúa creciendo en popularidad debido a su flexibilidad y escalabilidad, convirtiéndose en una opción clave para empresas que buscan una infraestructura personalizada y controlada."
 ];
+
 ?>
