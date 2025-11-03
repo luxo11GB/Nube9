7d5a211d91e302a1072081f74020d57084ea8644
luxo11GB <gzalesluis3@gmail.com>
2025-11-03 18:30:19 -0500

    Update training section with new course details

--- api/secciones/formacion.php
+++ api/secciones/formacion.php
@@ -4,8 +4,9 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $formacion = [
-"titulo" => "XXX",
-"autor" => "Gabinete de comunicación",
-"resumen" => "XXX",
+    "titulo" => "Cursos de formación profesional para jóvenes emprendedores",
+    "autor" => "Gabinete de comunicación",
+    "resumen" => "La universidad ofrece una serie de cursos de formación profesional dirigidos a jóvenes emprendedores, con el objetivo de mejorar sus habilidades en áreas clave como marketing, gestión empresarial y desarrollo de proyectos."
 ];
+
 ?>
