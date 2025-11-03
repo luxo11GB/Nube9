8765d31de14dc874960e9839225b7998487c3ff0
luxo11GB <gzalesluis3@gmail.com>
2025-11-03 18:29:11 -0500

    Update news details for UNS 50th anniversary

--- api/secciones/uns.php
+++ api/secciones/uns.php
@@ -4,8 +4,9 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $uns = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "La UNS celebra su 50 aniversario con una serie de eventos académicos y culturales",
+    "autor" => "Pedro Sánchez",
+    "resumen" => "La Universidad Nacional del Santa conmemora medio siglo de formación académica con conferencias, exposiciones y actividades culturales, reafirmando su compromiso con el desarrollo de la educación en la región."
 ];
+
 ?>
