92ebb97aa64d0041a0e61217df61f65f6c94b381
luxo11GB <gzalesluis3@gmail.com>
2025-11-03 18:25:38 -0500

    Update science news article details

--- api/secciones/ciencia.php
+++ api/secciones/ciencia.php
@@ -4,8 +4,9 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $ciencia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Descubren nueva especie de mamífero en la Amazonía",
+    "autor" => "Carlos Herrera",
+    "resumen" => "Un equipo de investigadores ha identificado una especie de mamífero previamente desconocida en la selva amazónica, lo que resalta la importancia de la conservación y el estudio de la biodiversidad en la región."
 ];
+
 ?>
