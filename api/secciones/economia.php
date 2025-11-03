5f20de9e706f7b947bb935ecc3d749736cc17715
luxo11GB <gzalesluis3@gmail.com>
2025-11-03 18:23:04 -0500

    Update economy news details in economia.php

--- api/secciones/economia.php
+++ api/secciones/economia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+ "titulo" => "Inflación global alcanza niveles históricos en 2025",
+    "autor" => "María López",
+    "resumen" => "La inflación mundial continúa en aumento, afectando los precios de alimentos, energía y servicios. Expertos recomiendan estrategias de ahorro e inversión para mitigar el impacto económico en los hogares."
 ];
 ?>
