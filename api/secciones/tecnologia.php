d518a5629cc26de45608a9b3618053c33ecfafa7
luxo11GB <gzalesluis3@gmail.com>
2025-11-03 18:24:56 -0500

    Update tecnologia.php

--- api/secciones/tecnologia.php
+++ api/secciones/tecnologia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $tecnologia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "La Inteligencia Artificial revoluciona la industria de software",
+    "autor" => "Ana Gómez",
+    "resumen" => "Las empresas de tecnología están adoptando herramientas de IA para optimizar procesos, mejorar la experiencia del usuario y acelerar la creación de software, generando un cambio significativo en la industria."
 ];
 ?>
