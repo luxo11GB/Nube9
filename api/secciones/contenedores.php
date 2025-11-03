e36fcaf5509411c38d0d2fd5b02e9377976b6d5c
luxo11GB <gzalesluis3@gmail.com>
2025-11-03 18:33:13 -0500

    Update news container details in contenedores.php

--- api/secciones/contenedores.php
+++ api/secciones/contenedores.php
@@ -4,8 +4,9 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $contenedores = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "El auge de los contenedores en la infraestructura moderna",
+    "autor" => "Luis Sánchez",
+    "resumen" => "Los contenedores se han convertido en una tecnología esencial para la implementación de aplicaciones escalables y portátiles. Herramientas como Docker y Kubernetes están revolucionando la forma en que las empresas gestionan la infraestructura y los despliegues."
 ];
+
 ?>
