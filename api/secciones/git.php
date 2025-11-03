e54acc5269853ac419864b2704313b693e6be837
luxo11GB <gzalesluis3@gmail.com>
2025-11-03 18:32:34 -0500

    Update Git article details in git.php

--- api/secciones/git.php
+++ api/secciones/git.php
@@ -4,8 +4,9 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $git = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Mejores prácticas para gestionar repositorios Git",
+    "autor" => "Ana García",
+    "resumen" => "Git es una herramienta fundamental para el desarrollo de software. Conocer las mejores prácticas, como el uso de ramas, commits claros y manejo adecuado de conflictos, puede mejorar significativamente el flujo de trabajo en equipos de desarrollo."
 ];
+
 ?>
