bbdfb18bcc427847fe64bc0755a76211d7c845fb
luxo11GB <gzalesluis3@gmail.com>
2025-11-03 18:28:33 -0500

    Update television news details in television.php

--- api/secciones/television.php
+++ api/secciones/television.php
@@ -4,8 +4,9 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $television = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "El regreso de la serie 'Stranger Things' emociona a los fans",
+    "autor" => "Sofía Ramírez",
+    "resumen" => "Después de una larga espera, 'Stranger Things' regresa con su cuarta temporada, trayendo nuevas aventuras, giros inesperados y la nostalgia que tanto caracteriza a la serie."
 ];
+
 ?>
