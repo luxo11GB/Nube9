e7d8a76f6842174f91101d023207813c5bc51880
luxo11GB <gzalesluis3@gmail.com>
2025-11-03 18:31:22 -0500

    Update sociedad.php with news article details

--- api/secciones/sociedad.php
+++ api/secciones/sociedad.php
@@ -4,8 +4,9 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $sociedad = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "El impacto de la tecnología en la vida social moderna",
+    "autor" => "Carlos Fernández",
+    "resumen" => "La rápida expansión de la tecnología ha transformado la vida social, alterando la forma en que nos relacionamos, trabajamos y nos comunicamos, generando tanto oportunidades como desafíos en el mundo moderno."
 ];
+
 ?>
