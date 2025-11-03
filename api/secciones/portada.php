bb043e15f6b714e675dc129fb7dd703cec03ee3f
luxo11GB <gzalesluis3@gmail.com>
2025-11-03 18:15:59 -0500

    Add files via upload

---
+++ api/secciones/portada.php
@@ -0,0 +1,21 @@
+<?php
+/*****
+SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
+*****/
+
+$portada = [
+    "titulo" => "Gonzales Berrocal Luis Angel",
+    "autor" => "jperez",
+    "resumen" => "Las noticias mas importantes del 2025",
+];
+?>
+
+<style>
+  /* Cambia el fondo de la sección portada */
+  body {
+    background-color: #f0f8ff; /* color claro, cámbialo por el que quieras */
+  }
+</style>
+
+
+
