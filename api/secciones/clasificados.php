de4031945d3e8f178f9d6ec7dfa818796a3cc066
luxo11GB <gzalesluis3@gmail.com>
2025-11-03 18:34:08 -0500

    Replace placeholder values in clasificados.php
    Updated classified ad details with new job opportunities.

--- api/secciones/clasificados.php
+++ api/secciones/clasificados.php
@@ -4,8 +4,9 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $clasificados = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Oportunidades de empleo en tecnología para 2025",
+    "autor" => "Redacción de Clasificados",
+    "resumen" => "Se abren nuevas oportunidades laborales en el sector tecnológico, especialmente en áreas como desarrollo de software, ciberseguridad y análisis de datos, con una alta demanda de profesionales capacitados para el futuro cercano."
 ];
+
 ?>
