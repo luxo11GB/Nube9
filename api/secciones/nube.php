28e74e87671c69498c23e4f3a8dee368ae5e6a21
luxo11GB <gzalesluis3@gmail.com>
2025-11-03 18:27:11 -0500

    Update news article details in nube.php

--- api/secciones/nube.php
+++ api/secciones/nube.php
@@ -4,8 +4,9 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nube = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Empresas adoptan soluciones en la nube para 2025",
+    "autor" => "Carlos Mendoza",
+    "resumen" => "Cada vez más compañías migran sus servicios a la nube, buscando mayor escalabilidad, seguridad y eficiencia en la gestión de datos, lo que refleja un crecimiento sostenido en la adopción de tecnología cloud."
 ];
+
 ?>
