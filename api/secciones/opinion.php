fe4950e1b42a9b32d2f34ef6234fb342a460a2ee
luxo11GB <gzalesluis3@gmail.com>
2025-11-03 18:24:23 -0500

    Update opinion data with new article details

--- api/secciones/opinion.php
+++ api/secciones/opinion.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $opinion = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "El futuro del trabajo remoto en América Latina",
+    "autor" => "Carlos Fernández",
+    "resumen" => "La pandemia transformó la manera de trabajar y muchas empresas están evaluando modelos híbridos. Se analizan ventajas, desafíos y el impacto en la productividad y el bienestar de los empleados."
 ];
 ?>
