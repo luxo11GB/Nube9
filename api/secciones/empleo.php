20266a1fc95b03d5d205d82f277846521d9ac990
luxo11GB <gzalesluis3@gmail.com>
2025-11-03 18:30:50 -0500

    Update employment news details in empleo.php

--- api/secciones/empleo.php
+++ api/secciones/empleo.php
@@ -4,8 +4,9 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $empleo = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Nuevas oportunidades laborales en el sector tecnológico",
+    "autor" => "Laura Martínez",
+    "resumen" => "El sector tecnológico continúa creciendo y generando nuevas oportunidades laborales, con énfasis en perfiles especializados en inteligencia artificial, desarrollo web y ciberseguridad, abriendo puertas a jóvenes profesionales."
 ];
+
 ?>
