6b4040cd5f57ed8958fd9e4e18e7b7fd3a86205a
luxo11GB <gzalesluis3@gmail.com>
2025-11-03 18:26:26 -0500

    Replace placeholder news values with actual content
    Updated cultura.php with actual news details for the cultura category.

--- api/secciones/cultura.php
+++ api/secciones/cultura.php
@@ -4,8 +4,9 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $cultura = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Festival Internacional de Teatro celebra 50 años",
+    "autor" => "María López",
+    "resumen" => "El Festival Internacional de Teatro conmemora cinco décadas de historia reuniendo obras de diversos países, promoviendo el intercambio cultural y destacando nuevas propuestas artísticas."
 ];
+
 ?>
